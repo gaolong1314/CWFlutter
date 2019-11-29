295eae36b663e3447757670e673778ece4bd056b
gaolonglong <1430760748@qq.com>
2019-11-29 13:53:55 +0800

    flutter功能，里边带有flutterboost

---
+++ appflutter/build_for_ios/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <flutter_boost/BoostChannel.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [BoostChannel registerWithRegistrar:[registry registrarForPlugin:@"BoostChannel"]];
+}
+
+@end
